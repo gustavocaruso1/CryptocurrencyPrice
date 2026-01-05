d9793aa1f31fe2207b22c82838a238ca168f1243
gustavocaruso1 <gustavocaruso82@gmail.com>
2026-01-05 03:21:57 -0300

    Create Home Page

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+import arrow_icon from '../../assets/arrow_icon.png'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src={logo} alt="" className='logo'/>
+        <ul>
+            <li>Início</li>
+            <li>Features</li>
+            <li>Preços</li>
+            <li>Blog</li>
+        </ul>
+        <div className='nav-right'>
+            <select>
+                <option value="usd">USD</option>
+                <option value="brl">BRL</option>
+            </select>
+            <button>
+                Sign up <img src={arrow_icon} alt="" />
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
